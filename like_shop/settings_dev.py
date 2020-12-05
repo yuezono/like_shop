4a5d61e2c6868670892846f70a2fd451f4d2398b
yuezono <yuk.kgsm.ty02@gmail.com>
2020-12-05 13:44:56 +0900

    メール送信追加

---
+++ like_shop/settings_dev.py
@@ -0,0 +1,47 @@
+from .settings_common import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+#ロギング設定
+LOGGING = {
+    'version': 1, #１固定
+    'disable_existing_loggers': False,
+    #ロガーの設定
+    'loggers': {
+        #djangoが利用するロガー
+        'django': {
+            'handlers':['console'],
+            'level': 'INFO',
+        },
+        #shopsアプリケーションが利用するロガー
+        'shops': {
+            'handlers':['console'],
+            'level':'DEBUG',
+        },
+    },
+    #ハンドラの設定
+    'handlers':{
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter':'dev',
+        },
+    },
+    #フォーマッタの設定
+    'formatters':{
+        'dev':{
+            'format':'\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(Lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.consoke.EmailBackend'
--- shops/views.py
+++ shops/views.py
@@ -1,5 +1,11 @@
+import logging
+from django.urls import reverse_lazy
 from django.views import generic
 from .forms import InquiryForm
+
+from django.contrib import messages
+
+logger = logging.getLogger(__name__)
 
 class IndexView(generic.TemplateView):
   template_name = 'index.html'
@@ -7,3 +13,10 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
   template_name = 'inquiry.html'
   form_class = InquiryForm
+  success_url = reverse_lazy('shops:inquiry')
+
+  def form_valid(self,form):
+    form.send_email()
+    messages.success(self.request,'メッセージを送信しました')
+    logger.info('Inquiry sent by {}'.format(form,cleaned_data['name']))
+    return super().form_valid(form)
